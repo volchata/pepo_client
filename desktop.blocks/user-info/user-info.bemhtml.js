b4179715322d2781366d8ae9b669b0e13ab9d625
Pavel Smolnikov <smolnikovp@gmail.com>
2016-08-18 14:03:10 +0300

    сверстал страницу about

---
+++ desktop.blocks/user-info/user-info.bemhtml.js
@@ -0,0 +1,50 @@
+block('user-info')(
+    content()(
+        [
+            {
+                elem: 'header',
+                content: {
+                    block: 'top-menu',
+                    mods: { layout: 'main' }
+                }
+            },
+            {
+                elem: 'body',
+                content: [
+                    {
+                        block: 'text',
+                        mix: { block: 'user-info', elem: 'text'},
+                        content: 'Расскажите о себе'
+                    },
+                    {
+                        block: 'text',
+                        mix: { block: 'user-info', elem: 'text'},
+                        mods: { main: true },
+                        content: 'Чем вы отличаетесь от других?'
+                    }
+                ]
+            },
+            {
+                elem: 'edit',
+                content: [
+                    {
+                        block: 'text',
+                        content: 'О себе'
+                    },
+                    {
+                        block: 'textarea',
+                        mods: { theme: 'islands', size: 'm', 'has-clear': true },
+                        placeholder: 'Перечислите интересы'
+                    }
+                ]
+            },
+            {
+                elem: 'footer',
+                content: {
+                    block: 'bottom-menu',
+                    mods: { accept: true }
+                }
+            }
+        ]
+    )
+)
--- desktop.blocks/user-info/user-info.deps.js
+++ desktop.blocks/user-info/user-info.deps.js
@@ -2,13 +2,29 @@
     {
         block: 'user-info',
         mustDeps: [
-            { elem: 'edit' }
+            { elem: ['header', 'body', 'edit', 'footer', 'text'] }
         ]
     },
     {
         elem: 'edit',
         mustDeps: [
-            { block: 'input', mods: { theme: 'islands', size: 'm', 'has-clear': true }}
+            { block: 'textarea', mods: { theme: 'islands', size: 'm' }}
         ]
+    },
+    {
+        elem: 'body',
+        mustDeps: [
+            { block: 'text', mods: { main: true } }
+        ]
+    },
+    {
+        elem: 'header',
+        mustDeps: {
+            block: 'top-menu', mods: { layout: 'main' }
+        }
+    },
+    {
+        elem: 'footer',
+        mustDeps: { block: 'bottom-menu', mods: { accept: true }}
     }
 ])
