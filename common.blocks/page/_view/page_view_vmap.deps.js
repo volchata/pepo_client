8c70d1263d9c35027514d9ac418a1370586b6989
sasha <sasha@localhost.localdomain>
2016-09-06 14:14:06 +0300

    twits with maps

--- common.blocks/page/_view/page_view_vmap.deps.js
+++ common.blocks/page/_view/page_view_vmap.deps.js
@@ -35,23 +35,5 @@
                 tech: 'bemhtml'
             }
         ]
-    }])
-
-/*
- ({
- shouldDeps: [
- {
- block: 'top-menu',
- mods: { layout: 'main' }
- },
- {
- block: 'vmap-loader'
- },
- {
- block: 'vmap'
- }
- ]
- <<<<<<< e0c26de33f2d5ecbd79c70665d9b92c025506600
- })
- */
-
+    }
+]);
